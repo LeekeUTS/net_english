ff684ad63da970974536a896e1c31221b9d5ce32
Leeke <leeke.zhou@gmail.com>
2015-10-09 14:58:39 +1100

    1st

---
+++ net_english/Models/English.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace net_english.Models
+{
+    public class English
+    {
+        public int ID { get; set; }
+        public int PID { get; set; }
+        public String cn { get; set; }
+        public String google { get; set; }
+        public String trans { get; set; }
+        public String hit { get; set; }
+        public String notes { get; set; }
+        public String status { get; set; }
+    }
+
+    public class EnglishDBContext : DbContext
+    {
+        public DbSet<English> Englishs { get; set; }
+    }
+}
---
+++ net_english/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(net_english.Startup))]
+namespace net_english
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
